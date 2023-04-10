b1579ee9ce50b02442048a84cf7fa9ddc65fcd9a
suryanarayan02 <suryanarayankumar@virtualemployee.com>
2023-04-10 13:13:17 +0530

    vertical scroll

--- src/Screen/Signup/Signup.js
+++ src/Screen/Signup/Signup.js
@@ -50,6 +50,7 @@ const Signup = props => {
           backgroundColor: colors.white,
         }}>
         <KeyboardAwareScrollView
+          showsVerticalScrollIndicator={false}
           contentContainerStyle={{flexGrow: 1}}
           style={{
             flexGrow: 1,
